e4117fdd18e8cb237be885a104ad1d4b303d8373
dandevweb <danilo.pacheco@soitic.com>
2024-04-25 07:58:09 -0300

    04.object

---
+++ Music.cs
@@ -0,0 +1,7 @@
+﻿class Music
+{
+  public string name;
+  public string artist;
+  public int duration;
+  public bool available;
+}
--- Program.cs
+++ Program.cs
@@ -1,2 +1,13 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿Music music1 = new Music();
+music1.name = "The Sound of Music";
+music1.artist = "Julie Andrews";
+
+Console.WriteLine($"Nome da música: {music1.name}");
+Console.WriteLine($"Artista: {music1.name}");
+
+Music music2 = new Music();
+music2.name = "Vertigo";
+music2.artist = "U2";
+
+Console.WriteLine($"Nome da música: {music2.name}");
+Console.WriteLine($"Artista: {music2.name}");
