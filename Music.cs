b3ea0cf6dcd342df2bab2a7a0e3202812f1d6004
dandevweb <danilo.pacheco@soitic.com>
2024-04-25 08:04:11 -0300

    05.creating a method

--- Music.cs
+++ Music.cs
@@ -4,4 +4,13 @@
   public string artist;
   public int duration;
   public bool available;
+
+  public void ShowInfo()
+  {
+    Console.WriteLine($"Nome: {name}");
+    Console.WriteLine($"Artista: {artist}");
+    Console.WriteLine($"Duração: {duration}");
+    string isAvailable = available ? "Sim" : "Não";
+    Console.WriteLine($"Disponível no plano: {isAvailable}");
+  }
 }
--- Program.cs
+++ Program.cs
@@ -1,13 +1,14 @@
 ﻿Music music1 = new Music();
 music1.name = "The Sound of Music";
 music1.artist = "Julie Andrews";
-
-Console.WriteLine($"Nome da música: {music1.name}");
-Console.WriteLine($"Artista: {music1.name}");
+music1.duration = 173;
+music1.available = true;
 
 Music music2 = new Music();
 music2.name = "Vertigo";
 music2.artist = "U2";
+music2.duration = 189;
+music2.available = false;
 
-Console.WriteLine($"Nome da música: {music2.name}");
-Console.WriteLine($"Artista: {music2.name}");
+music1.ShowInfo();
+music2.ShowInfo();
