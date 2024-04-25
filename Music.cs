df002247864e48f5a8dd8d40397148820a0731b9
dandevweb <danilo.pacheco@soitic.com>
2024-04-25 19:05:53 -0300

    methods and properties

--- Music.cs
+++ Music.cs
@@ -1,16 +1,18 @@
 ﻿class Music
 {
-  public string name;
-  public string artist;
-  public int duration;
-  public bool available;
+  public string Name { get; set; }
+  public string Artist { get; set; }
+  public int Duration { get; set; }
+  public bool Available { get; set; }
+  public string Description => $"A música {Name} de {Artist} tem duração de {Duration} segundos.";
+    
 
   public void ShowInfo()
   {
-    Console.WriteLine($"Nome: {name}");
-    Console.WriteLine($"Artista: {artist}");
-    Console.WriteLine($"Duração: {duration}");
-    string isAvailable = available ? "Sim" : "Não";
+    Console.WriteLine($"Nome: {Name}");
+    Console.WriteLine($"Artista: {Artist}");
+    Console.WriteLine($"Duração: {Duration}");
+    string isAvailable = Available ? "Sim" : "Não";
     Console.WriteLine($"Disponível no plano: {isAvailable}");
   }
 }
--- Program.cs
+++ Program.cs
@@ -1,14 +1,15 @@
 ﻿Music music1 = new Music();
-music1.name = "The Sound of Music";
-music1.artist = "Julie Andrews";
-music1.duration = 173;
-music1.available = true;
+music1.Name = "The Sound of Music";
+music1.Artist = "Julie Andrews";
+music1.Duration = 173;
+music1.Available = true;
+Console.WriteLine(music1.Description);
 
 Music music2 = new Music();
-music2.name = "Vertigo";
-music2.artist = "U2";
-music2.duration = 189;
-music2.available = false;
+music2.Name = "Vertigo";
+music2.Artist = "U2";
+music2.Duration = 189;
+music2.Available = false;
 
 music1.ShowInfo();
 music2.ShowInfo();
